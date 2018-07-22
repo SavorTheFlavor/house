49b10c0617522c77d2c0cafef25654dd6bf1af3c
SavorTheFlavor <2500364530@qq.com>
2018-07-22 23:22:53 +0800

    集成freemarker
    expose-request-attributes: true ....

--- src/main/java/com/me/houses/controller/UserController.java
+++ src/main/java/com/me/houses/controller/UserController.java
@@ -3,22 +3,30 @@ package com.me.houses.controller;
 import com.me.houses.model.User;
 import com.me.houses.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 /**
  * Created by Administrator on 2018/7/22.
  */
-@RestController
+@Controller
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @RequestMapping("/users")
-    public List<User> getUsers(){
+    public @ResponseBody List<User> getUsers(){
         return userService.getUsers();
+    }
+
+    @RequestMapping("/hello")
+    public String hello(ModelMap modelMap){
+        modelMap.put("user", userService.getUsers().get(0));
+        return "hello";
     }
 }
