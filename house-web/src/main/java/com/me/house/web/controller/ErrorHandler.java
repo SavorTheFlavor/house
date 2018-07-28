af544cfb20a74fae1b0d8c59038051159949a8ee
SavorTheFlavor <2500364530@qq.com>
2018-07-28 22:05:56 +0800

    error handling
    freemarker默认会分别把404/500的请求转到error\404.ftl, error\505.ftl
    
    也可自定义异常页面处理
    @ControllerAdvice
    @ExceptionHandler

---
+++ house-web/src/main/java/com/me/house/web/controller/ErrorHandler.java
@@ -0,0 +1,22 @@
+package com.me.house.web.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Administrator on 2018/7/28.
+ */
+@ControllerAdvice
+public class ErrorHandler {
+    private static final Logger logger = LoggerFactory.getLogger(ErrorHandler.class);
+
+    @ExceptionHandler(value = {Exception.class})
+    public String error500(HttpServletRequest request, Exception e){
+        logger.error(request.getRequestURI() + ": encounter 500: "+ e.getMessage());
+        return "error/500";
+    }
+}
