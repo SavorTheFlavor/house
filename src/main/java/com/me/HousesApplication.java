46943fe7c4f5f94bb44687af294c2392839a2331
SavorTheFlavor <2500364530@qq.com>
2018-07-28 14:47:25 +0800

    custom springboot AutoConfiguration
    大概有三种方式：
    1. 在SpringBootApplication所在的根目录或其子目录下直接定义AutoConfiguration；springboot会自动递归扫描其根目录下的@Configuration
    2. 在根目录以外的则需在spring.factories文件中注册EnableAutoConfiguration
    3. 自定义注解，在该注解中@Import(...AutoConfiguration.class)

--- src/main/java/com/me/HousesApplication.java
+++ src/main/java/com/me/HousesApplication.java
@@ -2,8 +2,10 @@ package com.me;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import test.com.me.autoconfig.EnableHttpClient;
 
 @SpringBootApplication
+@EnableHttpClient
 public class HousesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HousesApplication.class, args);
